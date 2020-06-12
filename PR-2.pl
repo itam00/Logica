38fc3ccee07ce25933843b89b0cb5bc02c669304
mati <matyrios2015@gmail.com>
2020-06-12 14:18:02 -0300

    estructura basica agregada

--- PR-2.pl
+++ PR-2.pl
@@ -2,4 +2,16 @@
 :- op(300,fx,@). %operador que marca que un elemento debe aumentar su tamaño
 
 
+desplazar(Dir,Num,Cant,Tablero,EvolTablero):- guardarTablero(Tablero),mover(Dir,Num,Cant),combinarElementos(EvolTablero).
+
+% guarda todos los elementos de la lista como predicados de la forma
+% tabla/3
+guardarTablero(_):-dynamic tablero/3.
+
+%realiza el desplazamiento correspondiente sobre el tablero almacenado
+mover(_,_,_).
+
+
+combinarElementos(_).
+
 
